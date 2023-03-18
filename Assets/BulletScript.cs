079d735cd3507d68bd825535905960e13b2d252f
Anuj Umesh Yadav <anujy2689@gmail.com>
2023-03-18 14:39:43 +0530

    Animations and assets, gun reloading, not shooting while jumping

--- Assets/BulletScript.cs
+++ Assets/BulletScript.cs
@@ -7,17 +7,23 @@ public class BulletScript : MonoBehaviour
     public GameObject player;
     public float bulletSpeed = 50;
     public float direction;
+    public GameObject camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("player").GetComponent<GameObject>();
-        direction = player.transform.localScale.x;
+        player = GameObject.FindGameObjectWithTag("player");
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        direction = player.transform.localScale.x / 75;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += Vector3.right * bulletSpeed * Time.deltaTime * direction;
+        if ((transform.position.x - camera.transform.position.x) > 100 || (transform.position.x - camera.transform.position.x) < -100)
+        {
+            Destroy(gameObject);
+        }
     }
 }
